70e8f5d333275ffca5b1679a7f12046f1e2eb6b6
sunlar-3 <sunlar-3@student.ltu.se>
2023-11-14 14:06:48 +0100

    authTest expanded with possibility to insert to db

--- Code/authTest.py
+++ Code/authTest.py
@@ -2,39 +2,44 @@ import pymongo
 import sys
 from pymongo import MongoClient
 
+# For testing write username and password to skip input 
+# username = ""
+# password = ""
+
+# Input for auth
 print("Username : ")
 username = input()
 print("Password : ")
 password = input()
 
+
 cluster_url = "mongodb+srv://" + username + ":" + password + "@cluster0.qk8pnen.mongodb.net/"
-print("mongodb+srv://TestComp1:TestComp1@cluster0.qk8pnen.mongodb.net/ \n" + cluster_url)
 maxSevSelDelay = 1
 
+# Retriving the company database
 try:
     cluster = MongoClient(cluster_url)
     db = cluster["Test"]
     collection = db["dataTest"]
-    #collection = db["TestComp1"]
-    cluster.admin.command('ismaster')
+    d = collection.find_one({"CompName": username}).get("Database")
 except pymongo.errors.OperationFailure:
     print("Auth fail")
     sys.exit(1)
 
-retrievalDataObject = collection.find({},{'_id':0, 'CompID':0, 'CompName':0})
+# Input for new passport
+print("ItemName : ")
+ItemName = input()
+print("Origin : ")
+Origin = input()
 
-for data in retrievalDataObject:
-    s = data['Database']
-    print(s)
-
-    #retrievalData = s[13:len(data)-2]
-    
+# Retriving new itemID and insterting new passport to company database
 try:
-    collection = db[s]
-    post = {"ItemID":1, "ItemName":"Item1", "Origin":"Sweden",}
-    collection.insert_one(post)
+    collection = db[d]
+    retrievalDataObject = collection.find_one(sort=[("ItemID", pymongo.DESCENDING)])
+    newItemID = retrievalDataObject.get("ItemID") + 1
 
+    post = {"ItemID":newItemID, "ItemName":ItemName, "Origin":Origin,}
+    collection.insert_one(post)
 except pymongo.errors.OperationFailure:
     print("Auth fail2")
     sys.exit(1)
-
