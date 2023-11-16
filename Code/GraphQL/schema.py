36a31a103caa328610facadeec1b4bb0959440d4
elliot-eiksson <you@example.com>
2023-11-16 13:57:21 +0100

    Mutation to mongodb with python

--- Code/GraphQL/schema.py
+++ Code/GraphQL/schema.py
@@ -1,4 +1,3 @@
-
 import graphene
 from graphene.relay import Node
 from graphene_mongo import MongoengineConnectionField, MongoengineObjectType
@@ -11,23 +10,31 @@ class Item(MongoengineObjectType):
         model = CompanyModel
         interfaces = (Node, )
 
+
+
+class CreateItem(graphene.Mutation):
+    class Arguments:
+        item_id = graphene.Int(required=True)
+        item_name = graphene.String(required=True)
+        origin = graphene.String()
+
+    item = graphene.Field(lambda: Item)
+
+    def mutate(self, info, item_id, item_name, origin=None):
+        company = CompanyModel(ItemID=item_id, ItemName=item_name, Origin=origin)
+        company.save()
+        return CreateItem(item=company)
+
+class Mutation(graphene.ObjectType):
+    create_item = CreateItem.Field()
+
 class Query(graphene.ObjectType):
+    class Mutation(graphene.ObjectType):
+        create_item = CreateItem.Field()
     node = Node.Field()
 
+    all_item = MongoengineConnectionField(Item)    
 
-    all_item = MongoengineConnectionField(Item)
+schema = graphene.Schema(query=Query, mutation=Mutation, types=[Item])
 
-schema = graphene.Schema(query=Query)
-
-
-# class Item(graphene.ObjectType):
-#     ItemID = graphene.Int()
-#     ItemName = graphene.String()
-#     Origin = graphene.String()
-
-# class Query(graphene.ObjectType):
-#     item = graphene.Field(Item)
-
-
-# schema = graphene.Schema(query=Query)
     
