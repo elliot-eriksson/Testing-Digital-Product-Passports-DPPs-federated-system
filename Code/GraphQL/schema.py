da20ff33e83748ab02eda840192a7535ef273cd0
elliot-eriksson <elliot.eriksson@outlook.com>
2023-11-17 13:41:29 +0100

    Update works, from postman

--- Code/GraphQL/schema.py
+++ Code/GraphQL/schema.py
@@ -11,6 +11,23 @@ class Item(MongoengineObjectType):
         interfaces = (Node, )
 
 
+class UpdateItem(graphene.Mutation):
+    class Arguments:
+        item_id = graphene.ID(required = True)
+        name =  graphene.String()
+        origin = graphene.String()
+        
+    item = graphene.Field(lambda: Item)
+
+    def mutate(self, info, item_id, name = None, origin = None):
+        company = CompanyModel.objects.get(id=item_id)
+        if name is not None:
+            company.ItemName = name
+        if origin is not None:
+            company.Origin = origin
+        
+        company.save()
+        return UpdateItem(item=company)
 
 class CreateItem(graphene.Mutation):
     class Arguments:
@@ -27,10 +44,12 @@ class CreateItem(graphene.Mutation):
 
 class Mutation(graphene.ObjectType):
     create_item = CreateItem.Field()
+    update_item = UpdateItem.Field()
 
 class Query(graphene.ObjectType):
     class Mutation(graphene.ObjectType):
         create_item = CreateItem.Field()
+        update_item = UpdateItem.Field()
     node = Node.Field()
 
     all_item = MongoengineConnectionField(Item)    
