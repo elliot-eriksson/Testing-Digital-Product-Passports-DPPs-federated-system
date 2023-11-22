cfa5d706b053639587c2a194c9fbdf7ae0b73029
Limpowitch <timrosendahl2@gmail.com>
2023-11-22 12:34:29 +0100

    Fixed imports in update & auth

---
+++ Code/QRCode/qr_code.py
@@ -0,0 +1,29 @@
+import segno
+import sys
+import pymongo
+import sys
+import os
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+
+parent_dir = os.path.join(current_dir, '..')
+sys.path.append(parent_dir)
+
+from Authtest.authTest import authentication
+
+db, d, username = authentication()
+
+print("ItemID for QR creation : ")
+itemID = int(input())
+
+try:
+    collection = db[d]
+    retrievalDataObject = collection.find_one({"ItemID":itemID})
+    itemName = retrievalDataObject.get("ItemName")
+    origin = retrievalDataObject.get("Origin")
+except pymongo.errors.OperationFailure:
+    print("Auth fail2")
+    sys.exit(1)
+
+qrcode = segno.make_qr("ItemID = " + str(itemID) + "\nItemName = " + itemName + "\nOrigin = " + origin)
+qrcode.save("qrcode_" + username + "_" + str(itemID) + ".png", scale = 5)
--- Code/Update/update.py
+++ Code/Update/update.py
@@ -3,8 +3,15 @@ import sys
 from bson import ObjectId
 from pymongo import MongoClient
 
-from authTest import authentication
+import sys
+import os
 
+current_dir = os.path.dirname(os.path.realpath(__file__))
+
+parent_dir = os.path.join(current_dir, '..')
+sys.path.append(parent_dir)
+
+from Authtest.authTest import authentication
 
 db, d = authentication()
 print(db)
