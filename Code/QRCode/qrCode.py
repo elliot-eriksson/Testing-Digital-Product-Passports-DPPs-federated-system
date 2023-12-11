bd66b97af5acaaa53db2d5587084c39f7214cb1a
elliot-eriksson <elliot.eriksson@outlook.com>
2023-12-11 14:07:24 +0100

    Qrscanner with JSON format and a link

--- Code/QRCode/qrCode.py
+++ Code/QRCode/qrCode.py
@@ -1,14 +1,8 @@
 import segno
-import pymongo
 import sys
-import os
-
-current_dir = os.path.dirname(os.path.realpath(__file__))
-
-parent_dir = os.path.join(current_dir, '..')
-sys.path.append(parent_dir)
-
-from Authtest.authTest import authentication
+import json
+import pymongo
+from authTest import authentication
 
 db, d, username = authentication()
 
@@ -20,9 +14,26 @@ try:
     retrievalDataObject = collection.find_one({"ItemID":itemID})
     itemName = retrievalDataObject.get("ItemName")
     origin = retrievalDataObject.get("Origin")
+    LinkMadeFrom = str(retrievalDataObject.get("LinkMadeFrom"))
+    LinkMakes = str(retrievalDataObject.get("LinkMakes"))
+    #Det är så här vi vill ha de senare
+   # link = retrievalDataObject.get("IPFS")
 except pymongo.errors.OperationFailure:
-    print("QR Error")
+    print("Auth fail2")
     sys.exit(1)
 
-qrcode = segno.make_qr("ItemID = " + str(itemID) + "\nItemName = " + itemName + "\nOrigin = " + origin)
+#Dictionary created with data
+dataDictionary = {
+    "ItemID": itemID,
+    "ItemName": itemName,
+    "Origin": origin,
+    "LinkMadeFrom": LinkMadeFrom,
+    "LinkMakes": LinkMakes
+    
+}
+#Kommentera bort när link är implementerad
+link = "https://hadea.ec.europa.eu/calls-proposals/digital-product-passport_en"
+
+jsonData = json.dumps(dataDictionary)
+qrcode = segno.make_qr(jsonData +"\n"+ link)
 qrcode.save("qrcode_" + username + "_" + str(itemID) + ".png", scale = 5)
