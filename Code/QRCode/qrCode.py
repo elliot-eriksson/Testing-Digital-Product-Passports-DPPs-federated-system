3605bfa77e9af5946630917efe0d1b8f607a5926
SuneLarsson <boinkswe@gmail.com>
2023-12-13 12:21:36 +0100

    Fix authTest qrcode

--- Code/QRCode/qrCode.py
+++ Code/QRCode/qrCode.py
@@ -2,7 +2,14 @@ import segno
 import sys
 import json
 import pymongo
-from authTest import authentication
+import os
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+
+parent_dir = os.path.join(current_dir, '..')
+sys.path.append(parent_dir)
+
+from Authtest.authTest import authentication
 
 db, d, username = authentication()
 
